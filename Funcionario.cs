8067a5530a757871ee5ef780fcbc23af1118cdab
Caio Lourenço <lourencocaio228@gmail.com>
2022-08-08 09:30:15 -0300

    Salario Medio

---
+++ Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salario
+{
+    internal class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario()
+        {
+        }
+
+        public Funcionario(string nome, double salario)
+        {
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public double Media(Funcionario s1, Funcionario s2)
+        {
+            return (s1.Salario + s2.Salario) / 2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            double salario = double.Parse(Console.ReadLine());
+            Funcionario funcionario = new Funcionario(nome, salario);
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            salario = double.Parse(Console.ReadLine());
+            Funcionario funcionario2 = new Funcionario(nome, salario);
+
+            Console.WriteLine("Salário médio = " + funcionario.Media(funcionario, funcionario2).ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
